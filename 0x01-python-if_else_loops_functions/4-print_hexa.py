7a94320c09a7c2eb21abbe51d20b40eafdd999f2
AfekhikuDonald <afekhikudonald@yahoo.com>
2022-11-29 10:45:54 -0500

    fresh updates

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python
+for i in range(0, 99):
+    print("{:d} = 0x{:1x}".format(i, i))
---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+for i in range(99):
+    print("{0:02d}".format(i), end=", ")
+print("{:d}".format(99))
