8e6eb45aad4634c1674fe3a419732880bddce025
AfekhikuDonald <afekhikudonald@yahoo.com>
2022-11-30 18:05:44 -0500

    updates

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+
+/**
+ * check_cycle - checks if a linked list has a cycle
+ * @list: the list to check
+ * Return: 0 if there is no cycle else return 1
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *head, *tail;
+
+	if (list == NULL)
+		return (0);
+	head = list;
+	tail = list;
+	while (head && tail && tail->next)
+	{
+		head = head->next;
+		tail = tail->next->next;
+		if (head == tail)
+			return (1);
+		else
+			return (0);
+	}
+	return (1);
+}
