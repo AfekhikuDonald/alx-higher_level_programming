b46d3ffe9d84605e83334dd5dce0a8fd0b8502cf
AfekhikuDonald <afekhikudonald@yahoo.com>
2022-12-01 09:39:23 -0500

    tasks

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    args = sys.argv[1:]
+    args_len = len(args)
+    i = 1
+    if args_len == 0:
+        print("{:d} arguments.".format(args_len))
+    elif args_len == 1:
+        print("{:d} arguments.".format(args_len))
+        print("{:d}: {:s}".format(i, sys.argv[1]))
+    else:
+        print("{:d} arguments.".format(args_len))
+        while i < args_len:
+            print("{:d}: {:s}".format(i, sys.argv[i]))
+            i += 1
