9a28fd94503408482ee8a6d6845bfd35b46c8fa9
AfekhikuDonald <afekhikudonald@yahoo.com>
2022-12-01 09:40:21 -0500

    tasks

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
         print("{:d}: {:s}".format(i, sys.argv[1]))
     else:
         print("{:d} arguments.".format(args_len))
-        while i < args_len:
+        while i < args_len + 1:
             print("{:d}: {:s}".format(i, sys.argv[i]))
             i += 1
