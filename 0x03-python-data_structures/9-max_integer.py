b19711719b3d39651c7fffb48b0acca18834835c
AfekhikuDonald <afekhikudonald@yahoo.com>
2022-12-04 10:36:33 -0500

    data structures in python

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    else:
+        maxxie = my_list[0]
+        for i in range(1, len(my_list) - 1):
+            if my_list[i] > maxxie:
+                maxxie = my_list[i]
+                return maxxie
+            else:
+                return maxxie
+
