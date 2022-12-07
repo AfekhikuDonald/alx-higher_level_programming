56632776463a2e5ea16f27099b583020dec18c96
AfekhikuDonald <afekhikudonald@yahoo.com>
2022-12-07 19:14:02 +0000

    more data structures

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-	for i in my_list:
-		if i == search:
-			search = replace
+	for i in range(len(my_list) - 1):
+		if my_list[i] == search:
+			my_list[i] = replace
