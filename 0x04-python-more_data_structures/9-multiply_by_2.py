1d51a4514a86b6b7962044be3c9f59267e3d9e63
AfekhikuDonald <afekhikudonald@yahoo.com>
2022-12-07 23:42:51 +0000

    more data structures

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     new_dict = a_dicttionary.copy()
-    for i in new_dict.keys:
+    for i in new_dict.keys():
         i *= 2
     return (new_dict)
