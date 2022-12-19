41b0a0665e0362a42933555d28fdc1e2796551b8
AfekhikuDonald <afekhikudonald@yahoo.com>
2022-12-19 16:30:31 -0500

    exceptions

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from __future__ import print_function
+import sys
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except Exception as x:
+        print("Exception: {}".format(x), file=sys.stderr)
+        return False
+    return True
