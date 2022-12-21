04413e3301ac784a108fa4fe9c7a58ae2b636fc3
AfekhikuDonald <afekhikudonald@yahoo.com>
2022-12-21 08:12:51 -0500

    classes and objects

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""create a class square"""
+
+
+class square:
+    """define the class"""
+    def __init__(self, size):
+        """set the size attribute"""
+        self.__size = size
