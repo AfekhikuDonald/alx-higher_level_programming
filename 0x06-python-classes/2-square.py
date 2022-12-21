88ad36ec356f0a5a7c85e923b579b1f391353331
AfekhikuDonald <afekhikudonald@yahoo.com>
2022-12-21 08:36:01 -0500

    classes and objects

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# 2-square.py
+""" create a square class"""
+
+
+class square:
+    """ define the square"""
+
+    def __init__(self, size=0):
+        """initialize the square attributes
+        Args:
+            size : the size of the new square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
