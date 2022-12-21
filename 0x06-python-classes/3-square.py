17bcfdb0b2e4190d08a912a064ecf8d162544163
AfekhikuDonald <afekhikudonald@yahoo.com>
2022-12-21 09:47:53 -0500

    classes and objects

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -8,7 +8,7 @@ class Square:
 
     def __init__(self, size=0):
         """initialise the class
-        Args: 
+        Args:
             size : the size of the square
         """
         if not isinstance(size, int):
